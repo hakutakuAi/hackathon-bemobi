1703a82509b7e7f8546aef8adcd62303b9bb1f27
ViniciosLugli <vinicioslugli@gmail.com>
2024-10-13 06:52:01 -0300

    ♻️ (page.tsx): refactor import statements and formatting for consistency and readability

--- dashboard/app/page.tsx
+++ dashboard/app/page.tsx
@@ -1,25 +1,25 @@
-'use client';
+'use client'
 
-import Image from "next/image";
-import Logo from "../app/assets/logo.svg";
-import { Button } from "@nextui-org/react";
-import { useEffect, useState } from "react";
+import Image from 'next/image'
+import Logo from '@/assets/logo.svg'
+import { Button } from '@nextui-org/react'
+import { useEffect, useState } from 'react'
 
 export default function Home() {
-  const [isClient, setIsClient] = useState(false);
+	const [isClient, setIsClient] = useState(false)
 
-  useEffect(() => {
-    setIsClient(true);
-  }, []);
+	useEffect(() => {
+		setIsClient(true)
+	}, [])
 
-  return (
-    <div className="flex bg-[#EDE9E2] justify-center gap-20 flex-col items-center justify-items-center min-h-screen font-[family-name:var(--font-geist-sans)]">
-      {isClient && (
-        <>
-          <Image src={Logo} alt="Logo hakutaku" />
-          <Button>Login</Button>
-        </>
-      )}
-    </div>
-  );
+	return (
+		<div className="flex bg-[#EDE9E2] justify-center gap-20 flex-col items-center justify-items-center min-h-screen font-[family-name:var(--font-geist-sans)]">
+			{isClient && (
+				<>
+					<Image src={Logo} alt="Logo hakutaku" />
+					<Button>Login</Button>
+				</>
+			)}
+		</div>
+	)
 }
