757b6451b42a8ce995ca03778f6808e4579fdded
ViniciosLugli <vinicioslugli@gmail.com>
2024-10-19 08:31:51 -0300

    ✨ (route.ts): add dynamic export to force dynamic rendering for the API route to improve performance and responsiveness

--- dashboard/app/api/integrations/infos/route.ts
+++ dashboard/app/api/integrations/infos/route.ts
@@ -2,6 +2,8 @@ import { NextResponse } from 'next/server'
 import QdrantSingleton from '@/services/qdrant'
 import { kv } from '@vercel/kv'
 
+export const dynamic = 'force-dynamic'
+
 export async function GET() {
 	const client = QdrantSingleton.getClient()
 
